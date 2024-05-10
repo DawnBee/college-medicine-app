26850e38f4efc869f55057a3377cddde4b4045c7
DawnBee <52739140+DawnBee@users.noreply.github.com>
2024-05-10 12:39:17 +0700

    Initial 'backend' setup

---
+++ backend/api/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Events(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	title = models.CharField(max_length=30)
+	image = models.ImageField(default="default.png", upload_to="events", null=True, blank=True)
+	description = models.TextField()
+	event_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = "Event"
+		verbose_name_plural = "Events"
+		ordering = ['-event_date']
