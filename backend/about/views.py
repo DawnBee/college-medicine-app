8ac9b3779996ca6be51fe7b5b70a0dcfa0d956e6
DawnBee <52739140+DawnBee@users.noreply.github.com>
2024-06-09 12:25:41 +0700

    Added 'Curriculum' section in 'About' page
    Closes #86

--- backend/about/views.py
+++ backend/about/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Benefactors, Values
-from .serializers import BenefactorSerializer, ValueSerializer
+from .models import Benefactors, Values, Curriculum
+from .serializers import BenefactorSerializer, ValueSerializer, CurriculumSerializer
 
 class BenefactorViewSet(viewsets.ModelViewSet):
 	queryset = Benefactors.objects.all()
@@ -9,3 +9,7 @@ class BenefactorViewSet(viewsets.ModelViewSet):
 class ValueViewSet(viewsets.ModelViewSet):
 	queryset = Values.objects.all()
 	serializer_class = ValueSerializer
+
+class CurriculumViewSet(viewsets.ModelViewSet):
+	queryset = 	Curriculum.objects.all()
+	serializer_class = CurriculumSerializer
---
+++ frontend/src/components/Curriculum.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { API_BASE_URL } from '../api'
+import axios from 'axios'
+
+const Curriculum = () => {
+    const [curriculum, setCurriculum] = useState([])
+
+    useEffect(() => {
+        axios.get(`${API_BASE_URL}/about/curriculum`)
+            .then(res => {
+                setCurriculum(res.data)
+            })
+            .catch(err => {
+                console.error('Error fetching curriculum file:', err)
+            })
+    }, [])
+
+  return (
+    <section className="curriculum-section">
+    <div className="layout-container">
+        <h2>Curriculum & Syllabus</h2>
+        <p>
+            Explore the comprehensive curriculum and detailed syllabus 
+            for the College of Medicine. This section provides essential 
+            information on course structures, academic requirements, 
+            and study materials designed to guide students through their 
+            medical education journey.
+        </p>
+        {curriculum.map((curr) => (
+            <a href={curr.pdf} target="_blank" rel="noopener noreferrer">
+                <button className="view-file-btn">
+                    <i className="fa-regular fa-file-pdf" />
+                    View File
+                </button>
+            </a>
+        ))}
+    </div>
+    </section>
+  )
+}
+
+export default Curriculum
